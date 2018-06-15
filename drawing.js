677ccbb9bbe6c132c5a9ea8599346882a447367c
Jack Watson-Hamblin <jackwatson-hamblin@FluffyAir.local>
2018-06-15 14:02:44 +1000

    Super basic version that draws the arcs

---
+++ drawing.js
@@ -0,0 +1,44 @@
+var start = new Point(10, view.center.y);
+var upOrDown = 1;
+var group = new Group();
+
+var usedNumbers = [0];
+var currentNumber = 0;
+var negNumber;
+var jump;
+var interval = 2;
+var through;
+var end;
+var heightChange;
+var color = new Color(0.7, 0.9, 0.4);
+
+for(var i = 1; i<=10000; i++) {
+	negNumber = currentNumber - i;
+	if (negNumber > 0 && usedNumbers.indexOf(negNumber) === -1) {
+		currentNumber -= i;
+		jump = -1 * i * interval;
+	} else {
+		currentNumber += i;
+		jump = i * interval;
+	}
+	if (upOrDown === -1 && jump < 0) {
+		heightChange = jump;
+	} else if (upOrDown === 1 && jump < 0) {
+		heightChange = jump * -1;
+	} else if (upOrDown === -1 && jump > 0) {
+		heightChange = jump * -1;
+	} else {
+		heightChange = jump;
+	}
+	through = start + [(jump / 2), (heightChange / 2)];
+	end = start + [jump, 0];
+	var path = new Path.Arc(start, through, end);
+	group.addChild(path);
+	start = end;
+	upOrDown = upOrDown * -1;
+	usedNumbers.push(currentNumber);
+	color.hue = color.hue + 0.4;
+	path.strokeColor = color;
+}
+
+
